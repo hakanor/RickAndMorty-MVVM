70040ca42679f4ba283c2fee7cbe2056e0fcb77e
hakanor <hakanor99@gmail.com>
2023-04-24 14:11:38 +0300

    splash screen designed

--- usg-cekirge-challenge/SplashViewController.swift
+++ usg-cekirge-challenge/SplashViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 class SplashViewController: UIViewController {
     
     // MARK: - Subviews
+    private lazy var logo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "portal.png")
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .black
+        label.textColor = UIColor(red: 154/255, green: 195/255, blue: 90/255, alpha: 1)
         label.numberOfLines = 1
-        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 44, weight: .bold)
         label.text = "Welcome"
         return label
     }()
@@ -39,11 +50,16 @@ class SplashViewController: UIViewController {
     
     // MARK: - Helper Functions
     private func configureUI(){
-        view.backgroundColor = .yellow
-        [titleLabel] .forEach(view.addSubview(_:))
+        view.backgroundColor = .white
+        [logo, titleLabel] .forEach(view.addSubview(_:))
+        
+        let screen = UIScreen.main.bounds
+        
+        logo.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 24, paddingLeft: 24, paddingRight: 24, height: screen.width)
+        
+        titleLabel.anchor(top: logo.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 10, paddingLeft: 24,  paddingRight: 24)
         
         titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     private func checkFirstLaunch() {
